4c93e9fce589a710445e8df24e38c8c546673242
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-05-09 22:12:42 -0500

    env

---
+++ api/utils/config.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+};

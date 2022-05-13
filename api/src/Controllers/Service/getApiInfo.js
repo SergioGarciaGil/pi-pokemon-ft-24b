f4c349402407579521c6aa14e6f1db8d4a31dcac
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-05-12 23:01:48 -0500

    seervices getApiInfo

---
+++ api/src/Controllers/Service/getApiInfo.js
@@ -0,0 +1,40 @@
+
+const axios = require('axios');
+
+
+const getApiInfo = async () => {
+
+    try {
+        const callingApi = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=70")
+        const apiInfo = callingApi.data.results.map((el) => {
+            return axios.get(el.url)
+        })
+
+        const arrayPokes = await Promise.all(apiInfo).then((pokeArray) => {//Promise.all espera a que todas las promesas sean resultadas
+            const pokeArray = pokeArray.map((el) => {//el es el objeto que se esta iterando
+                return {
+
+                    id: element.data.id,
+                    name: element.data.name,
+                    hp: element.data.stats[0].base_stat,
+                    attack: element.data.stats[1].base_stat,
+                    defense: element.data.stats[2].base_stat,
+                    speed: element.data.stats[5].base_stat,
+                    height: element.data.height,
+                    weight: element.data.weight,
+                    types: element.data.types.map((element) => element.type),
+                    img: element.data.sprites.other.home.front_default,
+
+                }
+            });
+            return pokeArray;// retorna el array de objetos
+        });
+        return arrayPokes;// retorna el array de pokemones
+    } catch (error) {
+        console.log(error);// si hay un error se imprime en consola
+
+    }
+}
+
+
+module.exports = getApiInfo();
